b56ad776aa7a89ccb35a9be2d04d960b04ca0643
Jing Flint <jingflint@gmail.com>
2020-04-29 23:29:31 -0700

    76 done

--- 76MinimumWindowSubstring.py
+++ 76MinimumWindowSubstring.py
@@ -17,47 +17,48 @@ class Solution:
     # index = 0 1 2 3 4 5 6 7 8 9 10 11 12
     #        "A D O B E C O D E B A  N  C", t = "ABC" ----> use count_dict = {A: 0, B: 0, C: 0}
     #         i         j
-    # .              i   j
-    # Solution is not done
+    #               i   j
+    #               i             j
 
-    # Time O(N) Space O(1)
+    # Time O(N) Space O(N)
     def minWindow(self, s: str, t: str) -> str:
 
         if s is None or t is None: return None
         if s == [] or t == []: return ""
 
-        i, j, startIndex = 0, 0, 0
-        length = 0
+        i, start_i, end_j = 0, 0, 0
+        # t_dict = count_dict = {A: 1, B: 1, C: 1}
+        t_dict = collections.Counter(t)
+        missing_char_no = len(t_dict)
+        window_len = float('inf')
         count_dict = {}
         for c in t:
+            # count_dict = {A: 0, B: 0, C: 0}
             count_dict[c] = 0
 
-        while i <= j and j < len(s):
+        for j in range(len(s)):
 
-            if s[j] not in count_dict:
-                j += 1
-            if s[i] not in count_dict:
-                i += 1
-            else:
-
+            if s[j] in t:
                 count_dict[s[j]] += 1
-                hasAllChars = True
-                for v in count_dict.values():
-                    if v < 1:
-                        hasAllChars = False
-                if hasAllChars == True:
-                    rangeSize = len(s[i:j + 1])
-                    if length = 0:
-                        length = rangeSize
-                        startIndex = i
-                    elif rangeSize < length:
-                        length = rangeSize
-                        startIndex = i
+                if count_dict[s[j]] == t_dict[s[j]]:
+                    missing_char_no -= 1
+
+                while missing_char_no == 0:
+                    if window_len > j - i + 1:
+                        window_len = j - i + 1
+                        start_i = i
+                        end_j = j
+
+                    if s[i] in count_dict:
+                        count_dict[s[i]] -= 1
+                        if count_dict[s[i]] < t_dict[s[i]]:
+                            missing_char_no += 1
+
                     i += 1
 
-                else:
-                    print('there')
-                    j += 1
+        if window_len == float('inf'): return ''
+        return s[start_i:end_j + 1]
 
-        return s[startIndex:startIndex + length]
+
+
 
