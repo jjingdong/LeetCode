5a1e55d8b7042c60ed17bac8d542004b09bd27af
Jing Flint <jingflint@gmail.com>
2020-05-08 20:16:46 -0700

    797 done

---
+++ 797AllPathsFromSourceToTarget.py
@@ -0,0 +1,44 @@
+'''
+797. All Paths From Source to Target
+Medium
+
+Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+Example:
+Input: [[1,2], [3], [3], []]
+Output: [[0,1,3],[0,2,3]]
+Explanation: The graph looks like this:
+0--->1
+|    |
+v    v
+2--->3
+There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Note:
+	•	The number of nodes in the graph will be in the range [2, 15].
+	•	You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+'''
+
+
+class Solution:
+
+    # Time O(N) Space O(N)
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        paths = []
+
+        queue = collections.deque()
+        queue.append((0, []))
+
+        while len(queue) > 0:
+
+            node, path = queue.popleft()
+            if node == len(graph) - 1:
+                path.append(node)
+                paths.append(path)
+
+            for neighbor in graph[node]:
+                # Note. this has to be path + [node], it cannot use path.append(node)
+                queue.append((neighbor, path + [node]))
+
+        return paths
