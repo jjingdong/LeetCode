ff3d6fd714db9e95c3c3dccbba13be4ad53f54ae
Jing Flint <jingflint@gmail.com>
2020-05-11 13:52:41 -0700

    done 73 better way

--- 73SetMatrixZeros.py
+++ 73SetMatrixZeros.py
@@ -38,7 +38,43 @@ Follow up:
 
 class Solution:
 
-    # Time O(MNK), K = M+N Space O(1)
+    # Solution I
+    #
+    # Solution II
+
+    # Time O(MN) Space O(1), Solution II
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+
+        if matrix is None: return None
+
+        size = len(matrix)
+        col_size = len(matrix[0])
+        clear_first_col = False
+
+        for i in range(size):
+            if matrix[i][0] == 0:
+                clear_first_col = True
+            for j in range(1, col_size):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+        for c in range(1, col_size):
+            if matrix[0][c] == 0:
+                for r in range(size):
+                    matrix[r][c] = 0
+
+        for r in range(size):
+            if matrix[r][0] == 0:
+                matrix[r] = [0] * col_size
+
+        if clear_first_col == True:
+            for i in range(size):
+                matrix[i][0] = 0
+
+
+'''
+    # Time O(MNK), K = M+N Space O(1), Solution I
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
@@ -63,5 +99,9 @@ class Solution:
             for j in range(col_size):
                 if matrix[i][j] is None:
                     matrix[i][j] = 0
+'''
+
+
+
 
 
