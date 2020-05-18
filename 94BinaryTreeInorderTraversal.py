90704b32996547d6ea2bf6d1e5a870fc8b857e41
Jing Flint <jingflint@gmail.com>
2020-05-17 19:00:01 -0700

    94 done

---
+++ 94BinaryTreeInorderTraversal.py
@@ -0,0 +1,76 @@
+'''
+94. Binary Tree Inorder Traversal
+Medium
+
+2790
+
+120
+
+Add to List
+
+Share
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    # Solution I: Recursion
+    #
+    # Solution II: Iteration
+
+    # Time O(N) Space O(N), using iteration
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+
+        if not root: return []
+
+        in_order = []
+        stack = collections.deque([])
+        node = root
+        while node or stack:
+            while node is not None:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            in_order.append(node.val)
+            node = node.right
+
+        return in_order
+
+
+'''
+    # Time O(N) Space O(N), using recursion
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+
+        if not root: return []
+
+        def traverse(node): 
+            if node is None: return
+
+            traverse(node.left)
+            in_order.append(node.val)
+            traverse(node.right)
+
+        in_order = []
+        traverse(root)
+        return in_order
+'''
