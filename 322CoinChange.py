2b490244631d26193f08bcbeca3e3f4e1ed1a9ac
Jing Flint <jingflint@gmail.com>
2020-06-07 16:01:12 -0700

    322 done

--- 322CoinChange.py
+++ 322CoinChange.py
@@ -16,31 +16,44 @@ Note: You may assume that you have an infinite number of each kind of coin.
 
 class Solution:
 
-    # Solution I: Brute Force
-    #
-    # Solution II: Dynamic Programming - Memorization
-    #
-    # Solution III: Dynamic Programming - Tabulation
+    #         0 1 2 3 4 5 6 7 8 9 10 11
+    #     1   0 1 2 3 4 5 6 7 8 9 10 11
+    #     2   0 1 1 2 2 3 3 4 4 5 5  6
+    #     5   0 1 1 2 2 1
+    #     dp[i][j] = min(dp[i-1][j], dp[i][j-c] + 1)
 
-    # Note. this is not finished
-    # Time O() Space O()
+    # Time O(N^2) Space O(N), runtime = 1260 ms
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        def change(cur_sum, coin):
-            nonlocal count
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
 
-            if cur_sum > amount:
-                return
-            if cur_sum == amount:
-                count += 1
-                return
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i - c] + 1)
 
-            cur_sum += coin
-            count += 1
+        if dp[-1] == float('inf'): return -1
+        return dp[-1]
 
-            for c in coins:
-                change(amount, c)
 
-        count = 0
-        change(0, 0)
-        return count
+'''
+    # Time O(N^2) Space O(N^2), runtime = 2432 ms
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        size = len(coins)
+        dp = [[float('inf') for _ in range(amount+1)] for _ in range(size)]
+        for j in range(size):
+            dp[j][0] = 0
+
+        for i in range(size):
+            c = coins[i]
+            for j in range(1, amount+1):
+                if j >= c:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-c] + 1)
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        if dp[-1][-1] == float('inf'): return -1         
+        return dp[-1][-1]                
+'''
+
