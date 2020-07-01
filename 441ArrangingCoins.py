26cde878e7b4fad3295a781640f3085d887389d8
Jing Flint <jingflint@gmail.com>
2020-07-01 00:40:33 -0700

    441 done

--- 441ArrangingCoins.py
+++ 441ArrangingCoins.py
@@ -30,7 +30,43 @@ Because the 4th row is incomplete, we return 3.
 
 class Solution:
 
-    # Time O(K), 2^K=N, K=logN  Space O(1)
+    #                n = 8
+    #
+    # ¤              1     level = 1
+    # ¤ ¤            2     level = 2
+    # ¤ ¤ ¤          3     level = 3
+    # ¤ ¤
+    #
+    #         1 + 2 + 3 + 4 >= 8
+    # level   1   2   3
+    #
+    # If each stair is full, total = 1 + 2 + 3 + 4 + ... = k * (k+1) // 2
+    #                            k = 1   2   3   4
+    #   k * (k+1) // 2 >= n
+    #
+    #
+
+    # Time O(logN) Space O(1)
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 0, n
+        k = None
+        curr = None
+        while left <= right:
+
+            k = (right + left) // 2
+            curr = k * (k + 1) // 2
+            print(f'left = {left} right = {right} k = {k} curr = {curr}')
+            if curr == n:
+                return k
+            if n < curr:
+                right = k - 1
+            else:
+                left = k + 1
+        return right
+
+
+'''
+    # Time O(level) Space O(1)
     def arrangeCoins(self, n: int) -> int:
 
         if n < 0: return -1
@@ -41,4 +77,17 @@ class Solution:
             level += 1
 
         return level - 1
+'''
 
+'''
+    # Time O(level) Space O(1), runtime = 832 ms
+    def arrangeCoins(self, n: int) -> int:
+        num, level, total = 0, 0, 0
+        while total < n:
+            num += 1
+            total += num
+            level += 1
+
+        if total == n: return level
+        return level - 1
+'''
