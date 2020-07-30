763f0105454495472ce4635ebc6c9971225bea77
Jing Flint <jingflint@gmail.com>
2020-07-29 23:05:38 -0700

    modify stock

--- 121BestTimeToBuyAndSellStock.py
+++ 121BestTimeToBuyAndSellStock.py
@@ -1,57 +1,65 @@
-# 121. Best Time to Buy and Sell Stock
-# Easy
-#
-# Say you have an array for which the ith element is the price of a given stock on day i.
-#
-# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-#
-# Note that you cannot sell a stock before you buy one.
-#
-# Example 1:
-#
-# Input: [7,1,5,3,6,4]
-# Output: 5
-# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-#              Not 7-1 = 6, as selling price needs to be larger than buying price.
-# Example 2:
-#
-# Input: [7,6,4,3,1]
-# Output: 0
-# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
+121. Best Time to Buy and Sell Stock
+Easy
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
+
+class Solution:
+
+    # Time O(N)
+    # Space O(1)
+    def maxProfitII(self, prices: List[int]) -> int:
+
+        minPrice = prices[0]
+        maxProfit = 0
+
+        for i in range(len(prices)):
+            if prices[i] < minPrice:
+                minPrice = prices[i]
+            elif prices[i] - minPrice > maxProfit:
+                maxProfit = prices[i] - minPrice
+
+        return maxProfit
 
 
-#--------------------------------------------------------------------
-# Time O(N)
-# Space O(N)
-def maxProfit(self, prices: List[int]) -> int:
-    globalMax = 0
+'''
+    #Time O(N)
+    #Space O(N)
+    def maxProfit(self, prices: List[int]) -> int:
 
-    for i in range(len(prices)):
-        curPrice = prices[i]
+        globalMax = 0
 
-        if i == 0:
-            localDiff = 0
-            globalDiff = 0
-            globalMax = 0
-        else:
-            localDiff = curPrice - prices[i - 1]
+        for i in range(len(prices)):
+            curPrice = prices[i]
 
-        globalDiff = max(localDiff, globalDiff + localDiff)
-        globalMax = max(globalMax, globalDiff)
+            if i == 0:
+                localDiff = 0
+                globalDiff = 0
+                output = 0
+                globalMax = 0
+            else:
+                localDiff = curPrice - prices[i-1]
 
-    return globalMax
+            globalDiff = max(localDiff, globalDiff + localDiff) 
+            globalMax = max(globalMax, globalDiff)
 
+        return globalMax
+'''
 
-# Time O(N)
-# Space O(1)
-def maxProfitII(self, prices: List[int]) -> int:
-    minPrice = prices[0]
-    maxProfit = 0
-
-    for i in range(len(prices)):
-        if prices[i] < minPrice:
-            minPrice = prices[i]
-        elif prices[i] - minPrice > maxProfit:
-            maxProfit = prices[i] - minPrice
-
-    return maxProfit
--- 122BestTimeToBuyAndSellStockII.py
+++ 122BestTimeToBuyAndSellStockII.py
@@ -1,53 +1,71 @@
-# 122. Best Time to Buy and Sell Stock II
-# Easy
-#
-# Say you have an array for which the ith element is the price of a given stock on day i.
-#
-# Design an algorithm to find the maximum profit. You may complete as many transactions as
-# you like (i.e., buy one and sell one share of the stock multiple times).
-#
-# Note: You may not engage in multiple transactions at the same time (i.e., you must sell
-# the stock before you buy again).
-#
-# Example 1:
-#
-# Input: [7,1,5,3,6,4]
-# Output: 7
-# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
-#              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
-# Example 2:
-#
-# Input: [1,2,3,4,5]
-# Output: 4
-# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-#              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
-#              engaging multiple transactions at the same time. You must sell before buying again.
-# Example 3:
-#
-# Input: [7,6,4,3,1]
-# Output: 0
-# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
+122. Best Time to Buy and Sell Stock II
+Easy
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as
+you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell
+the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
 
 class Solution:
 
     # [7, 1, 5, 3, 6, 4]
     #     v  p  v  p
-
+    #
     # [7, 6, 5, 4, 3, 2]
     #                 v
-
+    #
     # [1, 2, 3, 4, 5, 6]
     #  v              p
+    #
+    # p = peak v = valley
 
-    # Time O(N)
-    # Space O(1)
+    # Time O(N) Space O(1), runtime = 52 ms
+    def maxProfit(self, prices: List[int]) -> int:
+
+        if not prices: return 0
+
+        profit = 0
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i - 1]
+            if diff > 0:
+                profit += diff
+
+        return profit
+
+
+'''  
+    #Time O(N)
+    #Space O(1)
     def maxProfit(self, prices: List[int]) -> int:
 
         if prices is None: return None
         if prices == []: return 0
 
-        result = 0
-
+        profit = 0
         v = prices[0]
         p = prices[0]
         diff = 0
@@ -58,7 +76,7 @@ class Solution:
             diff = prices[i] - prices[i - 1]
             if i == len(prices) - 1:
                 after = 0
-            else:
+            else: 
                 after = prices[i + 1] - prices[i]
 
             if diff < 0:
@@ -67,14 +85,12 @@ class Solution:
             if diff >= 0:
                 if i == len(prices) - 1:
                     p = prices[i]
-                    result += p - v
+                    profit += p - v
                 elif after < 0:
                     p = prices[i]
-                    result += p - v
-
-                # print("v = " + str(v) + "p = " + str(p))
+                    profit += p - v
 
         return result
-
+'''
 
 
