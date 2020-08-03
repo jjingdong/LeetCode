8730df6685aff6782f7dc6cd0905e55eb2798220
Jing Flint <jingflint@gmail.com>
2020-08-03 16:31:16 -0700

    125 done

--- 125ValidPalindrome.py
+++ 125ValidPalindrome.py
@@ -1,51 +1,83 @@
-# 125. Valid Palindrome
-# Easy
-#
-# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-#
-# Note: For the purpose of this problem, we define empty string as valid palindrome.
-#
-# Example 1:
-#
-# Input: "A man, a plan, a canal: Panama"
-# Output: true
-# Example 2:
-#
-# Input: "race a car"
-# Output: false
+'''
+125. Valid Palindrome
+Easy
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+'''
 
 class Solution:
 
-    # Input: "A man, a plan, a canal: Panama"
-    #         i = 0
-    #                                      j = len - 1
-    # Output: true
+    # Note. \w is same as [a-zA-Z0-9_]
 
-    # Time O(N/2)
-    # Space O(1)
+    # Time O(N) Space O(1) runtime = 120 ms
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        pattern = '[a-zA-Z0-9]'
+        s = ''.join(c for c in s if re.fullmatch(pattern, c))
+        return s == s[::-1]
+
+
+'''
+    # Time O(N) Space O(N), runtime = 40 ms
     def isPalindrome(self, s: str) -> bool:
 
-        if s is None: return False
-        if s == []: return True
-        if len(s) == 1: return True
+        s = s.lower()
+        s = ''.join(c for c in s if c.isalnum())     
+        return s == s[::-1]
+'''
 
-        length = len(s)
+'''
+    # Time O(N) Space O(1), runtime = 180 ms
+    def isPalindrome(self, s: str) -> bool:
 
-        i = 0
-        j = length - 1
+        i,j = 0, len(s)-1
+        pattern = '[a-zA-Z0-9]'
         while i <= j:
 
-            curI = s[i]  # 0
-            curJ = s[j]  # p
-
-            if not curI.isalnum():
-                i += 1  # i = 1
-            elif not curJ.isalnum():
-                j -= 1
-            elif curI.lower() != curJ.lower():
-                return False
-            elif curI.lower() == curJ.lower():
+            if re.fullmatch(pattern, s[i]) and re.fullmatch(pattern, s[j]):
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not re.fullmatch(pattern, s[i]):
                 i += 1
+            elif not re.fullmatch(pattern, s[j]):
+                j -= 1   
+
+        return True
+'''
+
+'''
+    # Time O(N) Space O(1), runtime = 52 ms
+    def isPalindrome(self, s: str) -> bool:
+
+        if not s: return True
+
+        i, j = 0, len(s) - 1
+        while i <= j:
+
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not s[i].isalnum():
+                i +=1
+            elif not s[j].isalnum():
                 j -= 1
 
         return True
+'''
