d6f350e1c9614a3e8209244bb3248ba9668f3c65
Jing Flint <jingflint@gmail.com>
2020-08-03 21:52:48 -0700

    65 done

--- 65ValidNumber.py
+++ 65ValidNumber.py
@@ -51,3 +51,44 @@ class Solution:
         pattern = '\s*([+-]?((\d*\.\d+)|(\d+\.?\d*))(e[+-]?\d+)?)\s*'
 
         return re.fullmatch(pattern, s)
+
+
+'''
+DFA solution from Steven:
+
+var isNumber = function(s) {
+    const dfa = [false,
+                 {blank:  1, sign: 2, decimal: 3, number: 4, e: 0}, // 1.blank
+                 {blank:  0, sign: 0, decimal: 3, number: 4, e: 0}, // 2.sign
+                 {blank:  0, sign: 0, decimal: 0, number: 6, e: 0}, // 3.decimal
+                 {blank: 10, sign: 0, decimal: 5, number: 4, e: 7}, // 4.number
+                 {blank: 10, sign: 0, decimal: 0, number: 6, e: 7}, // 5.decimal
+                 {blank: 10, sign: 0, decimal: 0, number: 6, e: 7}, // 6.number
+                 {blank:  0, sign: 8, decimal: 0, number: 9, e: 0}, // 7.e
+                 {blank:  0, sign: 0, decimal: 0, number: 9, e: 0}, // 8.sign
+                 {blank: 10, sign: 0, decimal: 0, number: 9, e: 0}, // 9.number
+                 {blank: 10, sign: 0, decimal: 0, number: 0, e: 0}] // 10.blank
+    let curr = 1;
+    for (let i = 0; i < s.length; i++) {
+        if (!curr) return false;
+        let key
+        const char = s[i];
+        if ('+-'.includes(char)){
+            key = 'sign';
+        } else if ('0123456789'.includes(char)) {
+            key = 'number';
+        } else if (char === 'e') {
+            key = 'e';
+        } else if (char === '.') {
+            key = 'decimal'
+        } else if (char === ' ') {
+            key = 'blank'
+        } else {
+            return false;
+        }
+        curr = dfa[curr][key];
+    }
+    return [10,9,6,5,4].includes(curr)
+};
+
+'''
