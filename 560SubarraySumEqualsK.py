0ff83382f37b1cefe9489a9befaa45d1c4e247f1
Jing Flint <jingflint@gmail.com>
2020-08-17 22:42:10 -0700

    560 done

--- 560SubarraySumEqualsK.py
+++ 560SubarraySumEqualsK.py
@@ -2,14 +2,16 @@
 560. Subarray Sum Equals K
 Medium
 
-Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+Given an array of integers and an integer k, you need to find the total
+ number of continuous subarrays whose sum equals to k.
 Example 1: 
 Input:nums = [1,1,1], k = 2
 Output: 2
 
 Note: 
 	1.	The length of the array is in range [1, 20,000].
-	2.	The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+	2.	The range of numbers in the array is [-1000, 1000] and the range
+	of the integer k is [-1e7, 1e7].
 '''
 
 
@@ -18,7 +20,7 @@ class Solution:
     # Solution I: Cumulated Sum --- Time Limit exceeded
     # sum[i:j] = accumulated_sum[j] - accumulated_sum[i-1]
     #
-    # Solution II: HashMap ---> Didn't implement
+    # Solution II: HashMap
 
     # Time O(N) Space O(N), runtime = 140 ms
     def subarraySum(self, nums: List[int], k: int) -> int:
