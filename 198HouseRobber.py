e740cfb092340dbc063ab94b7bcb941f7d10ffca
Jing Flint <jingflint@gmail.com>
2020-09-14 21:53:10 -0700

    198HouseRobber.py

--- 198HouseRobber.py
+++ 198HouseRobber.py
@@ -22,36 +22,52 @@ class Solution:
     # pre:
     # global:1, 2, 4, 4
 
-    # E.g. [2, 7, 9, 3, 1]
+    # E.g.    [2, 7, 9, 3, 1]
     #         2, 2 or 7, 2+9 or 7, 3+7, or 11, 11+1 or 11
     # global: 2, 7, 11, 11, 12
 
-    # Time: O(N)
-    # Space: O(1)
+    # Time O(N) Space O(1), basically a fibnacchi
+    # runtime = 32 ms
     def rob(self, nums: List[int]) -> int:
 
         if nums is None: return None
         if nums == []: return 0
+        if len(nums) == 1: return nums[0]
 
-        preMax = nums[0]  # 2
-        result = nums[0]  # 2
-        prepre = 0  # 0
-
+        cur, pre, prepre = 0, nums[0], 0
         for i in range(1, len(nums)):
+            cur = max(pre, nums[i] + prepre)
+            prepre = pre
+            pre = cur
 
-            cur = nums[i]  # 7
-
-            if pre > cur + prepre:
-                result = pre  #
-
-            elif pre <= cur + prepre:
-                result = cur + prepre  # result = 7
-
-            prepre = pre  #
-            pre = result  #
-
-        return result
+        return cur
 
 
+'''                  
+    def rob_elliott(nums: List[int]) -> int:
+        dp = [0, 0]
+        for n in nums:
+            dp[0], dp[1] = dp[1], max(dp[0] + n, dp[1])
+        return dp[1]
 
+    def rob_gregory(nums: List[int]) -> int:
+        house_0, house_1 = 0, 0
+        for house in nums:
+            house_1, house_0 = max(house_0 + house, house_1), house_1
+        return house_1
 
+    def rob_hugh(nums: List[int]) -> int:
+        # 28ms beats 85%
+        # memory beats 48%
+        N = len(nums)
+        if N <= 2:
+            return N and max(nums)
+        nums[2] = max(nums[0] + nums[2], nums[1])
+        for i in range(3, N):
+            nums[i] += max(
+                nums[i - 3],
+                nums[i - 2],
+                nums[i - 1] - nums[i]
+            )
+        return nums[-1]
+'''
