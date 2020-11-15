54a7a747374840573cc4d481544176687b8c0ce1
Jing Flint <jingflint@gmail.com>
2020-11-15 01:15:45 -0800

    46 done

--- 46Permutations.py
+++ 46Permutations.py
@@ -20,11 +20,22 @@ Output:
 
 class Solution:
 
+    # Recursion Tree
+    #         1
+    #         |
+    #         1
+    #
+    #         1, 2
+    #     /         \
+    #   1,2         2,1
+    #
     #               1, 2, 3
-    #     i=0           i=1          i=2
-    #      1             2            3
-    #   i=1  i=2      i=1  i=2    i=1  i=2
-    #   12   13        21   23     31   32
+    #     i=0          i=1          i=2
+    #      1xx             2xx            3xx                1 digit in 1st position
+    #
+    #   i=1  i=2      i=0  i=2         i=0  i=1
+    #   12x   13x        21x   23x     31x   32x            1 digit in 2nd position
+    #
     #   i=2  i=1      i=2  i=0    i=1  i=0
     #   123  132      213  231    312  321
 
@@ -43,6 +54,5 @@ class Solution:
         results = []
         mutation([], nums)
         return results
-
 
 
