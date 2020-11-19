429407e98399df4cd03fa408e5fdab0e989d1429
Jing Flint <jingflint@gmail.com>
2020-11-18 23:16:21 -0800

    98 done

---
+++ 98ValidateBinarySearchTree.py
@@ -0,0 +1,60 @@
+'''
+98. Validate Binary Search Tree
+Medium
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+
+    # Time O(N) Space O(H)
+    # runtime = 48 ms
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        def traverse(node, lo, hi):
+
+            if not node: return True
+
+            if node.val >= hi: return False
+            if node.val <= lo: return False
+
+            L = traverse(node.left, lo, node.val)
+            R = traverse(node.right, node.val, hi)
+            return L and R
+
+        return traverse(root, float('-inf'), float('inf'))
