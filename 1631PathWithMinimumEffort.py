4e1c690522ebd9fa49cb4ed32df602463d60474d
Jing Flint <jingflint@gmail.com>
2021-02-03 19:58:41 -0800

    1631 done

--- 1631PathWithMinimumEffort.py
+++ 1631PathWithMinimumEffort.py
@@ -88,4 +88,77 @@ class Solution:
         dfs(0, 0, 0, heights[0][0])
         return global_max
 
+    # Solution II: Using Dijkstra
+    # Time O(MNlogMN) Space O(MN)
+    # runtime = 708 ms
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        size = len(heights)
+        col_size = len(heights[0])
+        visited = [[False for _ in range(col_size)] for _ in range(size)]
+
+        queue = [(0, 0, 0)]
+        while queue:
+            max_diff, r, c = heapq.heappop(queue)
+
+            if visited[r][c]:
+                continue
+
+            visited[r][c] = True
+            # print(f'r = {r} c = {c} max_diff = {max_diff} curcell = {heights[r][c]}')
+
+            if r == size - 1 and c == col_size - 1:
+                return max_diff
+
+            dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            for dr, dc in dirs:
+                rr, cc = r + dr, c + dc
+                if rr >= 0 and rr < size and cc >= 0 and cc < col_size and not visited[rr][cc]:
+                    new_max_diff = max(max_diff, abs(heights[r][c] - heights[rr][cc]))
+                    heapq.heappush(queue, (new_max_diff, rr, cc))
+
+        return None
+
+
+    '''
+        [[1,2,2],[3,8,2],[5,3,5]]
+            stdout
+            max_diff = 0 r = 0 c = 0
+            max_diff = 1 r = 0 c = 1
+            max_diff = 1 r = 0 c = 2
+            max_diff = 1 r = 1 c = 2
+            max_diff = 2 r = 1 c = 0
+            max_diff = 2 r = 2 c = 0
+            max_diff = 2 r = 2 c = 1
+            max_diff = 2 r = 2 c = 2
+    '''
+    # Solution III: Using Dijkstra
+    # Time O(MNlogMN) Space O(MN)
+    # runtime = 744 ms
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        size = len(heights)
+        col_size = len(heights[0])
+        visited = [[float('inf') for _ in range(col_size)] for _ in range(size)]
+
+        queue = [(0, 0, 0)]
+        visited[0][0] = 0
+        while queue:
+            max_diff, r, c = heapq.heappop(queue)
+
+            if r == size - 1 and c == col_size - 1:
+                return max_diff
+
+            dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            for dr, dc in dirs:
+                rr, cc = r + dr, c + dc
+                if rr >= 0 and rr < size and cc >= 0 and cc < col_size:
+                    new_max_diff = max(max_diff, abs(heights[r][c] - heights[rr][cc]))
+
+                    if new_max_diff < visited[rr][cc]:
+                        visited[rr][cc] = new_max_diff
+                        heapq.heappush(queue, (new_max_diff, rr, cc))
+
+        return None
+
 
