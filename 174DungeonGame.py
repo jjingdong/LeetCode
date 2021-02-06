8da5733518f11b0082e3621cbe4ae417dd01d4da
Jing Flint <jingflint@gmail.com>
2021-02-06 10:57:34 -0800

    174 done

--- 174DungeonGame.py
+++ 174DungeonGame.py
@@ -2,13 +2,22 @@
 174. Dungeon Game
 Hard
 
-The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
-The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
-Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
-In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+The demons had captured the princess (P) and imprisoned her in the bottom-right corner
+of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant
+knight (K) was initially positioned in the top-left room and must fight his way through
+the dungeon to rescue the princess.
+The knight has an initial health point represented by a positive integer. If at any point
+his health point drops to 0 or below, he dies immediately.
+Some of the rooms are guarded by demons, so the knight loses health (negative integers)
+upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that
+increase the knight's health (positive integers).
+In order to reach the princess as quickly as possible, the knight decides to move only
+rightward or downward in each step.
  
-Write a function to determine the knight's minimum initial health so that he is able to rescue the princess.
-For example, given the dungeon below, the initial health of the knight must be at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
+Write a function to determine the knight's minimum initial health so that he is able to
+rescue the princess.
+For example, given the dungeon below, the initial health of the knight must be at least 
+7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
 -2 (K)
 -3
 3
@@ -53,7 +62,7 @@ class Solution:
         return dp[0]
 
 
-'''
+
     # Time O(MN) Space O(1), runtime = 76 ms
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
 
@@ -72,4 +81,3 @@ class Solution:
                 dungeon[i][j] = max(1, min(dungeon[i+1][j]-dungeon[i][j], dungeon[i][j+1]-dungeon[i][j]))
 
         return dungeon[0][0]
-'''
