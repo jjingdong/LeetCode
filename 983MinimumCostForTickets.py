a5c0cdb53ac5d5d2464e51c21b60975d784bdc79
Jing Flint <jingflint@gmail.com>
2021-03-18 15:21:47 -0700

    983 done

--- 983MinimumCostForTickets.py
+++ 983MinimumCostForTickets.py
@@ -97,64 +97,42 @@ class Solution:
     #  cost 2   0   2           4       6   8   10
     #  cost 15  0
 
-    # Time O(N), N = 30, Space O(N) Note. can set the array to size 31. But I haven't implement
-    # runtime = 48 ms
+    # Time O(N) Space O(N)
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
 
-        a = 0
+        size = days[-1] + 1
+        dp = [float('inf')] * size
 
+        for i in range(0, days[0]):
+            dp[i] = 0
+        # print(dp)
 
-
-
-'''
-    # Time O(N), N = last day to travel + 1, Space O(N)
-    # runtime = 48 ms
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-
-        dp = [float('inf')] * (days[-1]+1)
-        for j in range(0, days[0]):
-            dp[j] = 0
-
-        temp = float('inf')
-        for i in range(days[0], days[-1]+1):
+        # days = [1,4,6,7,8,20]
+        # i = 0, 1, 2,3 , 4, 5, 6,7
+        for i in range(1, size):
+            # travel
+            # i = 1, 4, 6, 8, 20
             if i in days:
+                a = dp[i - 1] + costs[0]
+                b = costs[1]
+                c = costs[2]
+                if i > 7:
+                    b = dp[i - 7] + costs[1]
+                if i > 30:
+                    c = dp[i - 30] + costs[2]
 
-                b = dp[i-7]+costs[1] if i > 7 else costs[1]
-                c = dp[i-30]+costs[2] if i > 30 else costs[2]
+                dp[i] = min(a, b, c)
 
-                dp[i] = min(dp[i], dp[i-1]+costs[0], b, c)
-                temp = dp[i]
+            # not travel
             else:
-                dp[i] = temp
+                dp[i] = dp[i - 1]
+
+        # print(dp)
 
         return dp[-1]
-'''
-
-'''
-        Note. I couldnot come up with recursive solution    
-        def ticket(amount, day_index):
-            nonlocal min_amount
-
-            print(f'day = {day}, amount = {amount}')
 
 
-            # if day >= days[-1]:
-            #     min_amount = min(min_amount, amount)
-            #     return
 
-            ticket(day+1, amount+costs[0], day_index+1)
-            ticket(day+7, amount+costs[1], day_index+1)
-            ticket(day+30, amount+costs[2], day_index+1)
-
-
-        if not days: return 0
-
-        min_amount = float('inf')
-        pass_1, pass_7, pass_30 = 1, 7, 30
-        for i in range(len(days)):
-            ticket(day_index)
-        return min_amount
-'''
 
 
 
