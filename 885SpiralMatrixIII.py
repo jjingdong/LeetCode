274af6b4a2cd6e349c18f59ee529ec96436f89ff
Jing Flint <jingflint@gmail.com>
2021-07-27 17:23:49 -0700

    885 done

---
+++ 885SpiralMatrixIII.py
@@ -0,0 +1,67 @@
+'''
+885. Spiral Matrix III
+Medium
+
+You start at the cell (rStart, cStart) of an rows x cols grid facing east. The northwest corner
+is at the first row and column in the grid, and the southeast corner is at the last row and column.
+
+You will walk in a clockwise spiral shape to visit every position in this grid. Whenever you move
+outside the grid's boundary, we continue our walk outside the grid (but may return to the grid
+boundary later.). Eventually, we reach all rows * cols spaces of the grid.
+
+Return an array of coordinates representing the positions of the grid in the order you visited
+them.
+
+
+
+Example 1:
+
+
+Input: rows = 1, cols = 4, rStart = 0, cStart = 0
+Output: [[0,0],[0,1],[0,2],[0,3]]
+Example 2:
+
+
+Input: rows = 5, cols = 6, rStart = 1, cStart = 4
+Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],
+[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
+
+
+Constraints:
+
+1 <= rows, cols <= 100
+0 <= rStart < rows
+0 <= cStart < cols
+'''
+
+
+class Solution:
+
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+
+        matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+
+        r, c = rStart, cStart
+        result = [[r, c]]
+        size = len(matrix)
+        col_size = len(matrix[0])
+        step_size = 2 * max(size, col_size)
+
+        for s in range(1, step_size, 2):
+            # print(f's = {s}')
+            # right, down, left, up
+            dirs = [(0, 1, s), (1, 0, s), (0, -1, s + 1), (-1, 0, s + 1)]
+            for dr, dc, ds in dirs:
+
+                for _ in range(ds):
+                    r = r + dr
+                    c = c + dc
+                    # print(f'r = {r} c = {c}')
+
+                    if 0 <= r < size and 0 <= c < col_size:
+                        result.append([r, c])
+                        if len(result) == size * col_size:
+                            return result
+
+        return result
+
